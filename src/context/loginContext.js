929f0ede4cf14047bdfb2d1f24e4835ae4728864
bhargavi <bhargavibhoomika@gmail.com>
2021-07-13 23:11:40 +0530

    done

---
+++ src/context/loginContext.js
@@ -0,0 +1,10 @@
+
+
+import React from 'react'
+
+const LoginContext=React.createContext();
+const LoginProvider=LoginContext.Provider;
+const LoginConsumer=LoginContext.Consumer;
+
+export{LoginConsumer,LoginProvider}
+export default LoginContext
